f225b3ee2ead8283f4c40ecaa5119f892d522de5
lambotik <lambotik1988@gmail.com>
2024-02-26 22:48:23 +0300

    Update: Added: Many somthing ))

---
+++ tests_ui/main_page.py
@@ -0,0 +1,17 @@
+import allure
+import requests
+
+from tests_ui.base_page import BasePage
+from tests_ui.login_page_locators import Locators
+
+
+class MainPage(BasePage):
+    locators = Locators()
+
+    @allure.step('check_links')
+    def check_links(self):
+        links = self.elements_are_present(self.locators.LINKS)
+        list_links = [link.get_attribute('href') for link in links]
+        for link in list_links:
+            with allure.step(f'{link} status code is: {requests.get(link).status_code}'):
+                assert requests.get(link).status_code != 404, f'Link {link} is broken!'
